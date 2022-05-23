f683dfd263ec6fb077bdcbc12db21c1c92ced1b1
ghost4277 <ikkaev.david@mail.ru>
2022-05-23 21:08:42 +0300

    dsads

--- logIn/login.js
+++ logIn/login.js
@@ -1,6 +1,15 @@
 const formBtn = document.querySelectorAll('.form-btn')
 const formTab = document.querySelectorAll('.form-item')
+const btnClose = document.querySelector('.close-modal')
 
+btnClose.onclick = function () {
+    modal.style.display = 'none'
+}
+window.onclick = function (event) {
+    if (event.target == modal)
+        modal.style.display = 'none'
+
+}
 formBtn.forEach(function (item) {
     item.addEventListener('click', function () {
         let currentBtn = item
@@ -15,4 +24,17 @@ formBtn.forEach(function (item) {
         currentBtn.classList.add('active')
         currentTab.classList.add('active')
     })
+})
+document.querySelectorAll('.popup-link').forEach(element => {
+    element.addEventListener('click', (event) => {
+        event.preventDefault();
+        let id = element.getAttribute('href').replace('#', '')
+        document.getElementById(id).style.display = 'block'
+    })
+});
+let modal = document.querySelector('.modal-window')
+let mdoalBtn = document.querySelector('.popup-link')
+mdoalBtn.addEventListener('click', (event) => {
+    event.preventDefault();
+    modal.style.display = 'block'
 })
